41541f8b05dcc0da46bab886d2dadb27b5fe1d89
Yuval Allweil <themarquisdesheric@gmail.com>
2017-05-03 10:07:55 -0700

    Refactored server

--- server.js
+++ server.js
@@ -2,9 +2,9 @@
 const http = require('http');
 const app = require('./lib/app');
 
-const PORT = process.env.PORT || 3000;
 const server = http.createServer(app);
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-  console.log(`server running on port ${server.address()}`);
+  console.log('server running on port ', server.address().port);
 });
