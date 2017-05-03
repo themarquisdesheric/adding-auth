28326cf55ca9d92af649c3009ff5de302a23f38a
Yuval Allweil <themarquisdesheric@gmail.com>
2017-05-03 11:41:20 -0700

    POST test passing

---
+++ test/e2e/artwork.test.js
@@ -0,0 +1,28 @@
+const db = require('./_db');
+const request = require('./_request');
+const assert = require('chai').assert;
+
+describe('artwork API', () => {
+
+  before(db.drop);
+
+  it('initial GET should return empty array', () => {
+    return request.get('/artworks')
+      .then(res => res.body)
+      .then(art => assert.deepEqual(art, []));
+  });
+
+  let nude = { name: 'Nude Descending A Staircase', artist: 'Duchamp', medium: 'oil' };
+
+  it('POST should add a document', () => {
+    return request.post('/artworks')
+      .send(nude)
+      .then(res => res.body)
+      .then(saved => {
+        assert.ok(saved._id);
+
+        nude = saved;
+      });
+  });
+
+});
